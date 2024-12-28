d99e47ea8cfc05fe1ea5265291c5b2955d0c0e68
Chandu <chandusingh751@gmail.com>
2024-12-28 18:13:56 +0530

    change route agin for test

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,6 @@ Route::get('contact', function () {
     return view('contact');
 })->name('contact');
 
-Route::get('test', function () {
-    return view('test');
-})->name('test');
+Route::get('tests', function () {
+    return view('tests');
+})->name('tests');
